85e6b62926ba74ebd1dbfba0fa4922be1ff9c9ec
Ashley Giselle Lorenzo <92343168+ashleylozz02@users.noreply.github.com>
2022-05-10 15:52:42 -0400

    Update easy.js

---
+++ easy.js
@@ -0,0 +1,15 @@
+function excercise(workouts) {
+  return () => {
+    return `Today's exercise ${workouts}`;
+  };
+}
+
+// prints "Today's exercise: running"
+let run = excercise("running");
+let swim = excercise("swimming");
+
+console.log(swim());
+console.log(run());
+// prints "Today's exercise: running"
+
+// prints "Today's exercise: swimming"
